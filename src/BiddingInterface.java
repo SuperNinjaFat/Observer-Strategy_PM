4d6fb754c03e93934057d903e8b6b92cbee93898
MatthewSchwartzkopf <matthew.schwartzkopfm@mymail.champlain.edu>
2018-10-11 22:40:38 -0400

    Final Final - Final

--- src/BiddingInterface.java
+++ src/BiddingInterface.java
@@ -1,3 +1,17 @@
+/**
+ * Lab-03 Observer Pattern-Auction
+ * 
+ * Champlain College CSI-340, Fall 2018
+ * 
+ * Author: Matthew Schwartzkopf And Paul Lindberg Emails:
+ * matthew.schwartzkopf@mymail.champlain.edu,
+ * paul.lindberg@mymail.champlain.edu Class : CSI-340 Assignment: Lab 03
+ * Observer Pattern-Action Date Assigned: October 4, 2018 Due Date: October 11, 2018
+ * 
+ * Description:
+ * 
+ * This code represents the Observer Pattern using strategy patterns and UML for an Auction House/
+ **/
 
 public interface BiddingInterface {
 
